40ecf1622cdb565de6554bfddc1d0df424d2827f
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-08-09 16:17:13 +0200

    Updates

---
+++ precache-manifest.72aa770487f6a62106171e7e3704d21b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840d09f82eea55eddd83bf07ce6cb229",
+    "url": "/cashBakeApp/index.html"
+  },
+  {
+    "revision": "74570398f01c12953361",
+    "url": "/cashBakeApp/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "564bf1e6a9491be39ba3",
+    "url": "/cashBakeApp/static/css/main.506855ac.chunk.css"
+  },
+  {
+    "revision": "74570398f01c12953361",
+    "url": "/cashBakeApp/static/js/2.2194fd8b.chunk.js"
+  },
+  {
+    "revision": "564bf1e6a9491be39ba3",
+    "url": "/cashBakeApp/static/js/main.e7a50b47.chunk.js"
+  },
+  {
+    "revision": "a584dfbe75c504f60fb5",
+    "url": "/cashBakeApp/static/js/runtime~main.d45f00ff.js"
+  },
+  {
+    "revision": "f862badb34ea3362f3b7d0756383830b",
+    "url": "/cashBakeApp/static/media/costs.f862badb.svg"
+  },
+  {
+    "revision": "0027f4d633dfed7c1c64c839336943e5",
+    "url": "/cashBakeApp/static/media/logo.0027f4d6.svg"
+  },
+  {
+    "revision": "532bb59563a277ba72b1e2eeeada0abe",
+    "url": "/cashBakeApp/static/media/pig.532bb595.svg"
+  },
+  {
+    "revision": "8da0e5afab3f1ee73176f894d378ee76",
+    "url": "/cashBakeApp/static/media/revenues.8da0e5af.svg"
+  }
+]);
